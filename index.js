d2dbb9e8ce761f4390ee3a0dfeaec330b3c7e475
Joseph Murillo <josephmurillo2425@gmail.com>
2022-04-27 00:33:51 -0600

    cors library options whitelist

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
@@ -7,6 +8,14 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    whitelist.includes(origin) ? callback(null, true) : callback(new Error('no permitido'));
+  }
+}
+app.use(cors(options));
 
 routerApi(app);
 
